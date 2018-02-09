9cdef74b153ceca302c5306f5e87a56ad45bdc8c
john  kyony <jkyony@gmail.com>
2018-02-09 11:18:46 +0000

    the associations

--- app/models/deposit_paid.rb
+++ app/models/deposit_paid.rb
@@ -1,3 +1,4 @@
 class DepositPaid < ActiveRecord::Base
   belongs_to :project
+  validates :project_id, uniqueness: true
 end
--- app/models/feature_payment.rb
+++ app/models/feature_payment.rb
@@ -1,3 +1,4 @@
 class FeaturePayment < ActiveRecord::Base
   belongs_to :project_milestone
+  validates :project_milestone_id, uniqueness: true
 end
