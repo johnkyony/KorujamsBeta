542b55e5c1eb6fbda6ec4a3289eb4642706d6336
john  kyony <jkyony@gmail.com>
2018-02-09 00:02:55 +0000

    the next feature completed but buggy

--- app/controllers/pay_features_controller.rb
+++ app/controllers/pay_features_controller.rb
@@ -1,15 +1,31 @@
 class PayFeaturesController < ApplicationController
   before_action :set_project
+  before_action :set_feature_paying_for
+  before_action :set_feature_price
+  
+  
   def new
-   last_feature_paid_for = FeaturePayment.where(project_id: params[:project_id]).pluck(:project_milestone_id).last
-   last_feature = ProjectMilestone.order_features_at(ProjectMilestone.find(last_feature_paid_for))
-   @feature = last_feature.next
+    @feature
   end
 
   def create
+   @successful_paid_feature = FeaturePayment.create(project_milestone_id: @feature.id , amount: @feature_per_price , project_id: @project.id )
+   redirect_to project_project_milestones_path(@project.id)
   end
   private 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+  
+  def set_feature_paying_for
+   last_feature_paid_for = FeaturePayment.where(project_id: params[:project_id]).pluck(:project_milestone_id).last
+   last_feature = ProjectMilestone.order_features_at(ProjectMilestone.find(last_feature_paid_for))
+   @feature = last_feature.next
+    
+  end
+  
+  def set_feature_price
+    @feature_per_price = 1000
+    
   end
 end
