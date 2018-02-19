0fcdbe588eb20c7329e9622d7f8c03d7f4adad7a
john  kyony <jkyony@gmail.com>
2018-02-19 02:13:53 +0000

    the fix

--- app/controllers/personal_messages_controller.rb
+++ app/controllers/personal_messages_controller.rb
@@ -9,10 +9,9 @@ class PersonalMessagesController < ApplicationController
 
  def create
    
-   
     @conversation ||= Conversation.create(author_id: current_user.id,
                                           receiver_id: @receiver.id)
-          
+    
     @personal_message = current_user.personal_messages.build(personal_message_params)
     @personal_message.conversation_id = @conversation.id
      
